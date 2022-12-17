f76723c2a1e918327bf8226de4a610edf9cc1d70
Anjiang-Wei <43546559+Anjiang-Wei@users.noreply.github.com>
2022-12-16 17:36:15 -0800

    Update cp.py

--- cp.py
+++ cp.py
@@ -56,8 +56,12 @@ def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None, startco
     # save the workbook
     writer.save()
 
-def get_df_from_file(filename, sheetname):
-    return pd.read_excel(filename, sheet_name=sheetname, engine='openpyxl', header=None)
+def get_df_from_file(filename, sheetname=0):
+    if "csv" in filename:
+        return pd.read_csv(filename, header=None)
+    else:
+        return pd.read_excel(filename, sheet_name=sheetname, engine='openpyxl', header=None)
+        # return pd.read_excel(filename, sheet_name=sheetname, engine='openpyxl_wo_formatting', header=None)
 
 def get_val_from_df(df, row, col):
     return df.iloc[row, col]
@@ -71,13 +75,80 @@ def update_df(df, row, col, new_val):
 def write_df_to_file(df, filename, sheetname, start_row, start_col):
     append_df_to_excel(filename=filename, df=df, sheet_name=sheetname, startrow=start_row, startcol=start_col, 
                        truncate_sheet=False, header=None, index=False)
-    
-    
+
+def convert2csv(input_file_name, sheet_name=0):
+    outputfile_name = input_file_name.replace("xlsx", "csv")
+    if os.path.isfile(outputfile_name):
+        print(outputfile_name, "already exist. Skipping converting")
+    data_xls = get_df_from_file(input_file_name, sheet_name)
+    data_xls.to_csv(outputfile_name, encoding='utf-8', header=None, index=False)
+
+def convertfiles(input_file_list):
+    for fname in input_file_list:
+        convert2csv(fname)
+
 def start():
     source = get_df_from_file("2.xlsx", "dest")
     print(get_val_from_df(source, 9, 2))
     update_df(source, 9, 2, "overwrite")
     write_df_to_file(source, "2.xlsx", "dest", 9, 3)
+
+def step(data_csv, report_csv):
+    data = [] # list of sth
+    with open(data_csv, "r") as fin:
+        lines = fin.readlines()[1:] # list of strings
+        for i in range(len(lines)):
+            line = lines[i].strip()
+            splitted = line.split(",")
+            first, last = splitted[0], splitted[7]
+            if first == "":
+                continue
+            first, last = int(first), float(last)
+            data.append([first, last])
+            # print(first, last)
+
+    report = []
+    with open(report_csv, "r", errors='ignore') as fin:
+        lines = fin.readlines()
+        for i in range(len(lines)):
+            line = lines[i].strip()
+            if "Time - pressurize (s)" in line:
+               time_1 = line.split(",")[1]
+               time_1 = int(float(time_1))
+               assert "Time - end (s)" in lines[i+1]
+               time_2 = lines[i+1].strip().split(",")[1]
+               time_2 = int(float(time_2))
+               report.append([time_1, time_2])
+    # print(report)
+    # print(len(report))
+    for i in range(len(report)):
+        t1, t2 = report[i]
+        filter_data = list(filter(lambda x: x[0] >= t1 and x[0] <= t2, data))
+        # print(t1, t2, filter_data)
+        to_write = []
+        for first_last in filter_data:
+            first_last = list(map(str, first_last))
+            to_write.append(",".join(first_last) + "\n")
+        with open("out" + str(i) + ".csv", "w") as fout:
+            fout.writelines(to_write)
+
+def copy(inputf, outputf, sheet_name):
+    source = get_df_from_file(inputf)
+    write_df_to_file(source, outputf, sheet_name, 19, 0)
     
+
+def copy_files(output):
+    filelist=["out" + str(i) + ".csv" for i in range(0, 6)]
+    sheet_name = {0: "Ar_35_15",
+                  1: "H2_35_15",
+                  2: "CH4_35_15",
+                  3: "N2_35_15",
+                  4: "O2_35_15",
+                  5: "CO2_35_15"}
+    for i in range(len(filelist)):
+        copy(filelist[i], output, sheet_name[i])
+
 if __name__ == "__main__":
-    start()
+    convertfiles(["data.xlsx", "report.xlsx"])
+    step("data.csv", "report.csv")
+    copy_files(output="Permeation Test Template (Short).xlsm")
